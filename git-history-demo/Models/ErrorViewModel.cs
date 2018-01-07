72c5923a0bf2bfea62863e4393041afa3ef480e7
Lei Xu <leixu@lean-soft.cn>
2018-01-07 22:07:53 +0800

    添加项目文件。

---
+++ git-history-demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace git_history_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
